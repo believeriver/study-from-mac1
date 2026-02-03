da1df7691dc6321ef4e99fd97d5e6b8cb9fb3436
believeriver <nobuyuki.galois@gmail.com>
2026-02-04 05:16:28 +0900

    20260204 04 create my tools

--- python/basic/tools/01/main.py
+++ python/basic/tools/01/main.py
@@ -47,11 +47,32 @@ class CreateConfig(object):
                 self.devices_list.append(device)
 
     def create_new_config(self):
-        pass
+        key1 = 'hostname SW-2960L-8TS'
+        key2 = 'vlan 10,247'
+        key3 = 'switchport access vlan '
+        key4 = 'ip address 192.168.99.2 255.255.255.0'
+
+        for device in self.devices_list:
+            for item in self.template_config:
+                if key1 in item:
+                    self.created_config.append(f"hostname {device['HOSTNAME']}")
+                    continue
+                if key2 in item:
+                    self.created_config.append(f"vlan {device['DVLAN']} {device['MVLAN']}")
+                    continue
+                if key3 in item:
+                    self.created_config.append(f"switchport access vlan {device['DVLAN']}")
+                    continue
+                if key4 in item:
+                    self.created_config.append(f"ip address {device['IP']} 255.255.255.0")
+                    continue
+                self.created_config.append(item)
+            print(self.created_config)
 
     def run(self):
         self.import_template()
         self.import_input_list()
+        self.create_new_config()
 
 
 if __name__ == '__main__':
