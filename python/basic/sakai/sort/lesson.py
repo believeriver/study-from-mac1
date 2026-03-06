17b32295954c7f66656aa8e746d277504e090613
believeriver <nobuyuki.galois@gmail.com>
2026-03-07 08:57:40 +0900

    20260307 01

--- python/basic/sakai/sort/lesson.py
+++ python/basic/sakai/sort/lesson.py
@@ -4,15 +4,28 @@
 from typing import List
 
 
-def insertion_sort(numbers):
+def selection_sort(numbers):
     size = len(numbers)
     for i in range(size):
         index = i
         for j in range(i, size):
-            if numbers[i] > numbers[j]:
+            if numbers[index] > numbers[j]:
                 index = j
         numbers[i], numbers[index] = numbers[index], numbers[i]
 
+    return numbers
+
+
+def insertion_sort(numbers: List[int]) -> List[int]:
+    size = len(numbers)
+    for i in range(1, size):
+        temp = numbers[i]
+        j = i - 1
+        while j >= 0 and numbers[j] > temp:
+            # print(j, end=' ')
+            numbers[j+1] = numbers[j]
+            j -= 1
+        numbers[j+1] = temp
     return numbers
 
 
@@ -32,6 +45,7 @@ if __name__ == '__main__':
     nums = [1, 5, 28, 25, 100, 52, 27, 91, 22, 99]
     # bucket_sort(nums)
     print(nums)
+    # print(selection_sort(nums))
     print(insertion_sort(nums))
 
 
